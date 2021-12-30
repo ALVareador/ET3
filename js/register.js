c31a5629f6514554feaa8c78a528a119614505c7
Shinig4mi0mega <andres.garciadsr@gmail.com>
2021-12-30 18:20:29 +0100

    arreglado el register para que funcione
    arreglado el register para que funcione

--- js/register.js
+++ js/register.js
@@ -1,16 +1,25 @@
 function registrarUsuario() {
 
 	var idSession = getCookie('sessionId');
-	console.log(document.formularioRegistro);
 	insertacampo(document.formularioRegistro, 'ID_SESSION', idSession);
-	addActionControler(document.formularioRegistro, "add", "usuario");
+	addActionControler(document.formularioRegistro, "registrar", "AUTH");
 	console.log(document.formularioRegistro);
 	var idioma = getCookie('lang');
+
+	var formdata = $("#formularioRegistro").serialize();
+    formdata= decodeURIComponent(formdata.replace(/%2F/g,"/"));
+    var file = $("#foto_persona")[0].files[0];
+    var datos = new FormData();
+    datos.append("upload", file);
+    datos.append("formulario", formdata);
 
 	$.ajax({
 		method: "POST",
 		url: "http://193.147.87.202/ET3_IU/noRest.php",
-		data: $("#formularioRegistro").serialize(),
+		data: datos,
+        contentType: false,
+        processData: false,
+		
 	}).done(function (response) {
 		if (response.ok == true) {
 			respuestaOKAjax();
